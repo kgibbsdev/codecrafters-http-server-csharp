86128277c06adfc8b835d5850b2482c84f328ad5
kgibbsdev <kyletgibbs@gmail.com>
2026-02-20 22:04:07 -0500

    codecrafters submit [skip ci]

--- src/Server.cs
+++ src/Server.cs
@@ -1,3 +1,4 @@
+using System.Diagnostics;
 using System.IO.Compression;
 using System.Net;
 using System.Net.Sockets;
@@ -78,111 +79,126 @@ byte[] StringToByteArray(string input)
     return Encoding.ASCII.GetBytes(input);
 }
 
-void HandleRequest(Socket connection)
+void HandleConnection(Socket connection)
 {
     byte[] outBytes = new byte[1024];
-    
-    int numberOfBytes = connection.Receive(outBytes);
-    
-    string query = Encoding.ASCII.GetString(outBytes, 0, numberOfBytes);
-    
-    string[] requestLines = query.Split("\r\n");
-    
-    string httpMethod = requestLines[0].Split(" ")[0];
-    string httpTarget = requestLines[0].Split(" ")[1];
-    
-    string messageBody =  GetMessageBody(requestLines);
-    
-    if (httpMethod == "GET")
+    connection.SetSocketOption(SocketOptionLevel.Socket,  SocketOptionName.KeepAlive, true);
+    connection.SetSocketOption(SocketOptionLevel.Tcp, SocketOptionName.TcpKeepAliveTime, 3);
+
+    while (connection.Connected)
     {
-        var acceptEncodingValue = CheckForAcceptedEncoding(requestLines);
-        
-        if (httpTarget == "/")
+        try
         {
-            connection.Send(okResponse);
-        }
-        else if (httpTarget.StartsWith("/echo"))
-        {
-            //Skip to the part of the string after '/echo/'
-            int startingIndex = httpTarget.LastIndexOf("/") + 1;
-            string message = httpTarget.Substring(startingIndex);
-            byte[] byteResponse;
-            if (acceptEncodingValue.Contains("gzip"))
+            int numberOfBytes = connection.Receive(outBytes);
+
+            string query = Encoding.ASCII.GetString(outBytes, 0, numberOfBytes);
+
+            string[] requestLines = query.Split("\r\n");
+
+            string httpMethod = requestLines[0].Split(" ")[0];
+            string httpTarget = requestLines[0].Split(" ")[1];
+
+            string messageBody = GetMessageBody(requestLines);
+
+            if (httpMethod == "GET")
             {
-                byteResponse = CreateGzippedResponse(message);
+                var acceptEncodingValue = CheckForAcceptedEncoding(requestLines);
+
+                if (httpTarget == "/")
+                {
+                    connection.Send(okResponse);
+                }
+                else if (httpTarget.StartsWith("/echo"))
+                {
+                    //Skip to the part of the string after '/echo/'
+                    int startingIndex = httpTarget.LastIndexOf("/") + 1;
+                    string message = httpTarget.Substring(startingIndex);
+                    byte[] byteResponse;
+                    if (acceptEncodingValue.Contains("gzip"))
+                    {
+                        byteResponse = CreateGzippedResponse(message);
+                    }
+                    else
+                    {
+                        string stringResponse =
+                            $"HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {message.Length}\r\n\r\n{message}";
+                        byteResponse = Encoding.ASCII.GetBytes(stringResponse);
+                    }
+
+                    connection.Send(byteResponse);
+                }
+                else if (httpTarget.StartsWith("/user-agent"))
+                {
+                    var userAgentLine = requestLines.Where(l => l.StartsWith("User-Agent")).ToList()[0];
+                    var elements = userAgentLine.Split(' ');
+                    var userAgentName = elements[1];
+                    string stringResponse =
+                        $"HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {userAgentName.Length}\r\n\r\n{userAgentName}";
+                    byte[] byteResponse = StringToByteArray(stringResponse);
+                    connection.Send(byteResponse);
+                }
+                else if (httpTarget.StartsWith("/files"))
+                {
+                    // remove /files
+                    string fileName = httpTarget.Substring(7);
+                    string filePath = args[1] + fileName;
+                    Console.WriteLine("File Path: " + filePath);
+                    if (File.Exists(filePath))
+                    {
+                        string fileContent = File.ReadAllText(filePath);
+                        string stringResponse =
+                            $"HTTP/1.1 200 OK\r\nContent-Type: application/octet-stream\r\nContent-Length: {fileContent.Length}\r\n\r\n{fileContent}";
+                        byte[] byteResponse = StringToByteArray(stringResponse);
+                        connection.Send(byteResponse);
+                    }
+                    else
+                    {
+                        connection.Send(notFoundResponse);
+                    }
+
+                }
+                else
+                {
+                    connection.Send(notFoundResponse);
+                }
             }
-            else
+            else if (httpMethod == "POST")
             {
-                string stringResponse = $"HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {message.Length}\r\n\r\n{message}";
-                byteResponse =  Encoding.ASCII.GetBytes(stringResponse);
-            }
-            
-            connection.Send(byteResponse);
-        }
-        else if (httpTarget.StartsWith("/user-agent"))
-        {
-            var userAgentLine = requestLines.Where(l => l.StartsWith("User-Agent")).ToList()[0];
-            var elements = userAgentLine.Split(' ');
-            var userAgentName = elements[1];
-            string stringResponse =
-                $"HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {userAgentName.Length}\r\n\r\n{userAgentName}";
-            byte[] byteResponse = StringToByteArray(stringResponse);
-            connection.Send(byteResponse);
-        }
-        else if (httpTarget.StartsWith("/files"))
-        {
-            // remove /files
-            string fileName = httpTarget.Substring(7);
-            string filePath = args[1] +  fileName;
-            Console.WriteLine("File Path: " + filePath);
-            if (File.Exists(filePath))
-            {
-                string fileContent = File.ReadAllText(filePath);
-                string stringResponse =
-                    $"HTTP/1.1 200 OK\r\nContent-Type: application/octet-stream\r\nContent-Length: {fileContent.Length}\r\n\r\n{fileContent}";
-                byte[] byteResponse = StringToByteArray(stringResponse);
-                connection.Send(byteResponse);
+                if (httpTarget.StartsWith("/files"))
+                {
+                    // remove /files
+                    string fileName = httpTarget.Substring(7);
+                    string directory = "/tmp/";
+                    for (int i = 0; i < args.Length; i++)
+                    {
+                        if (args[i] == "--directory" && args.Length > (i + 1))
+                        {
+                            directory = args[i + 1];
+                        }
+                    }
+
+                    Directory.CreateDirectory(directory);
+                    File.WriteAllText(directory + fileName, messageBody);
+                    connection.Send(createdResponse);
+                }
+                else
+                {
+                    connection.Send(notFoundResponse);
+                }
             }
             else
             {
                 connection.Send(notFoundResponse);
             }
-            
         }
-        else
+        catch (SocketException e)
         {
-            connection.Send(notFoundResponse);
+            connection.Close();
         }
+
     }
-    else if (httpMethod == "POST")
-    {
-        if (httpTarget.StartsWith("/files"))
-        {
-            // remove /files
-            string fileName = httpTarget.Substring(7);
-            string directory = "/tmp/";
-            for (int i = 0; i < args.Length; i++)
-            {
-                if (args[i] == "--directory" && args.Length > (i + 1))
-                {
-                    directory = args[i + 1];
-                }
-            }
-            
-            Directory.CreateDirectory(directory);
-            File.WriteAllText(directory + fileName, messageBody);
-            connection.Send(createdResponse);
-        }
-        else
-        {
-            connection.Send(notFoundResponse);
-        }
-    }
-    else
-    {
-        connection.Send(notFoundResponse);
-    }
-    connection.Close();
+
+    // connection.Close();
 }
 
 
@@ -192,7 +208,7 @@ server.Start();
 while (true)
 {
     var connection = server.AcceptSocket(); // wait for client
-    Thread thread = new Thread(() => HandleRequest(connection));
+    Thread thread = new Thread(() => HandleConnection(connection));
     thread.Start();
 }
 
